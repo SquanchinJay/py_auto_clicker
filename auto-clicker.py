02d71141a2a4d08d61a5569b4fefc49bd478362a
Jay <allen.jeremiah555@gmail.com>
2019-11-10 04:05:35 -0500

    more delay

--- auto-clicker.py
+++ auto-clicker.py
@@ -3,7 +3,7 @@ import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
 
-delay = 0.01
+delay = 0.5
 button = Button.right
 start_stop_key = KeyCode(char='s')
 exit_key = KeyCode(char='q')
